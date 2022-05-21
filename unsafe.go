3beffbdb60acc8060a44940a7c12a67d8eb8ee2e
lemon-mint <ice.lemons.mint@gmail.com>
2022-05-22 06:57:01 +0900

    Use sysAllocOS, sysFreeOS

--- unsafe.go
+++ unsafe.go
@@ -2,8 +2,8 @@ package umem
 
 import "unsafe"
 
-//go:linkname sysAlloc runtime.sysAlloc
-func sysAlloc(n uintptr, sysStat *uint64) unsafe.Pointer
+//go:linkname sysAllocOS runtime.sysAllocOS
+func sysAllocOS(n uintptr) unsafe.Pointer
 
-//go:linkname sysFree runtime.sysFree
-func sysFree(v unsafe.Pointer, n uintptr, sysStat *uint64)
+//go:linkname sysFreeOS runtime.sysFreeOS
+func sysFreeOS(v unsafe.Pointer, n uintptr)
